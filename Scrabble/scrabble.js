b8f95faeb3b8ce7602988265f6b8f4776dead8db
stephane chelly <chellytheo@gmail.com>
2024-09-10 10:56:31 +0200

    Scrabble

--- Scrabble/scrabble.js
+++ Scrabble/scrabble.js
@@ -1,9 +1,6 @@
 const Scrabble = {
     set: new Map([
         ['a', 1], ['b', 3], ['c', 3], ['d', 2], ['e', 1], ['f', 4], ['g', 2],
-        ['h', 4], ['i', 1], ['j', 8], ['k', 10], ['l', 1], ['m', 2], ['n', 1],
-        ['o', 1], ['p', 3], ['q', 8], ['r', 1], ['s', 1], ['t', 1], ['u', 1],
-        ['v', 4], ['w', 10], ['x', 10], ['y', 10], ['z', 10]
     ])
 };
 
@@ -27,6 +24,6 @@ function Score() {
     motsAvecScores.sort((a, b) => a.score - b.score);
 
     const resultat = motsAvecScores.map(item => `${item.mot} (${item.score})`).join(', ');
-    document.getElementById('score').textContent = 'résultat des mot : ${resultat}';
+    document.getElementById('score').textContent = "résultat des mot : " + resultat;
 }
 
