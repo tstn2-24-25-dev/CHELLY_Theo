cad2d2b648e9e628f4fd3acaafb73c11758fec0c
stephane chelly <chellytheo@gmail.com>
2024-09-11 10:03:36 +0200

    inventaire3

--- Inventaire/inventaire.py
+++ Inventaire/inventaire.py
@@ -1,13 +1,11 @@
 import random
 import string
+import operator
 
-
-import random
-import string
-
-N = random.randint(10, 1000)
+N = random.randint(10, 1001)
 print(N)
 caractere_numero = {}
+
 
 for i in range(N):
     longueur = random.randint(4, 8)
@@ -16,3 +14,14 @@ for i in range(N):
         caractere_numero[caractere] = random.randint(1, 100)  
     numero = caractere_numero[caractere]
     print(f"{caractere} {numero}")
+
+
+article = {}
+for caractere, numero in caractere_numero.items():
+    if caractere in article:
+        article[caractere] += numero
+    else:
+        article[caractere] = numero
+
+caractere_max = max(article, key=article.get)
+print(f"{caractere_max}")
