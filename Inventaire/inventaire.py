cfc913feeca951626e8b063a0f633c27956b0c54
stephane chelly <chellytheo@gmail.com>
2024-09-11 09:28:24 +0200

    inventaire2

--- Inventaire/inventaire.py
+++ Inventaire/inventaire.py
@@ -2,11 +2,17 @@ import random
 import string
 
 
+import random
+import string
+
 N = random.randint(10, 1000)
 print(N)
+caractere_numero = {}
+
 for i in range(N):
     longueur = random.randint(4, 8)
     caractere = ''.join(random.choice(string.ascii_lowercase) for _ in range(longueur))
-    numero = random.randint(1, 100)
+    if caractere not in caractere_numero:
+        caractere_numero[caractere] = random.randint(1, 100)  
+    numero = caractere_numero[caractere]
     print(f"{caractere} {numero}")
-    
