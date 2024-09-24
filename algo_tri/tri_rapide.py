690d91ff64157093f9d6ad44e4b9dc85b1493247
stephane chelly <chellytheo@gmail.com>
2024-09-24 11:50:49 +0200

    tri rapide fin

--- algo_tri/tri_rapide.py
+++ algo_tri/tri_rapide.py
@@ -1,21 +1,30 @@
 #fonction tri
-def tri_rapide(tableau, debut, fin):
-    pivot = tableau [fin -1]
+def tri_rapide(tab, debut, fin):
+    pivot = tab[fin -1]
     curseur = debut
 
     for i in range(debut, fin -1):
-        if tableau[i] <= pivot:
-           tableau[i], tableau[curseur] = tableau[curseur], tableau[i]
+        if tab[i] <= pivot:
+           tab[i], tab[curseur] = tab[curseur], tab[i]
            curseur += 1 
-    tableau[curseur], tableau[fin -1] = tableau[fin -1], tableau[curseur]
+    tab[curseur], tab[fin -1] = tab[fin -1], tab[curseur]
     return curseur
 
 
 
 
+#tri
+def tri(tab, debut, fin):
+     if debut < fin:
+            pivot_index = tri_rapide(tab, debut, fin)
+            tri(tab , debut, pivot_index)
+            tri(tab , pivot_index + 1, fin)
+
+
+
 
 #test
-tableau =[4,8,2,10,1,9,7,6,3,5]
-print (f'tableau original : ', tableau)
-tri_rapide(tableau)
-print (f'tableau trié : ', tableau)
+tab =[4,8,2,10,1,9,7,6,3,5]
+print (f'tableau original : ', tab)
+tri(tab, 0, len(tab))
+print (f'tableau trié : ', tab)
