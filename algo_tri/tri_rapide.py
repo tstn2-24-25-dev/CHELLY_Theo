48db46c14c29fb054d73e409457c0c38b3553289
stephane chelly <chellytheo@gmail.com>
2024-09-24 11:56:30 +0200

    .

--- algo_tri/tri_rapide.py
+++ algo_tri/tri_rapide.py
@@ -14,11 +14,11 @@ def tri_rapide(tab, debut, fin):
 
 
 #tri
-def tri(tab, debut, fin):
+def triRapide(tab, debut, fin):
      if debut < fin:
             pivot_index = tri_rapide(tab, debut, fin)
-            tri(tab , debut, pivot_index)
-            tri(tab , pivot_index + 1, fin)
+            triRapide(tab , debut, pivot_index)
+            triRapide(tab , pivot_index + 1, fin)
 
 
 
@@ -26,5 +26,5 @@ def tri(tab, debut, fin):
 #test
 tab =[4,8,2,10,1,9,7,6,3,5]
 print (f'tableau original : ', tab)
-tri(tab, 0, len(tab))
+triRapide(tab, 0, len(tab))
 print (f'tableau trié : ', tab)
