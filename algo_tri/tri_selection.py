2c453dbe09d9add5abd3fb0e27d4cc9a13ebf894
stephane chelly <chellytheo@gmail.com>
2024-09-24 16:16:55 +0200

    tri

--- algo_tri/tri_selection.py
+++ algo_tri/tri_selection.py
@@ -10,13 +10,18 @@ def triSelection(tab):
         tab[i], tab[indice_minimum] = tab[indice_minimum], tab[i]
 
 
-def indiceMin(tab, j):
+
+
+def indiceMin(T, j):
     indice_minimum = j
 #trouver l'indice de la plus petite valeur à partir de l'indice j
-    for i in range(j, len(tab)):
-        if tab[i] < tab[indice_minimum]:
+    for i in range(j, len(T)):
+        if T[i] < T[indice_minimum]:
             indice_minimum = i
     return indice_minimum
+
+
+
 
 tableau = [4,8,2,10,1,9,7,6,3,5]
 print("Tableau original:", tableau)
